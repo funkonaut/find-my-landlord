e32813564d26b85edbe151b931b1217d0c6d3f76
Chris Correll <crcorrell@gmail.com>
2020-12-13 16:30:12 -0600

    analytics

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,15 +1,3 @@
-// Initialize Cloud Firestore through Firebase
-var firebaseConfig = {
-	apiKey: "AIzaSyAEmj8FBWhfhE_61AXBIVDqkUgbCcosJbQ",
-	authDomain: "find-my-landlord.firebaseapp.com",
-	databaseURL: "https://find-my-landlord.firebaseio.com",
-	projectId: "find-my-landlord",
-	storageBucket: "find-my-landlord.appspot.com",
-	messagingSenderId: "470878094978",
-	appId: "1:470878094978:web:ebc5b2350e17f590af569b",
-	measurementId: "G-C7MQBH19E6"
-};
-
 var firebaseConfig = {
     apiKey: "AIzaSyAEmj8FBWhfhE_61AXBIVDqkUgbCcosJbQ",
     authDomain: "fmll-atx.firebaseapp.com",
@@ -18,6 +6,7 @@ var firebaseConfig = {
     storageBucket: "fmll-atx.appspot.com",
     messagingSenderId: "984793536411",
     appId: "1:984793536411:web:4ace72d6c579cd1440333e"
+    measurementId: "G-W9PC20BSNV"
   };
 
 // Initialize Firebase
