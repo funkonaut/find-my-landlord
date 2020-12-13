6a10ce0212aa9a5d6ec76905ee1fa9c8a0d6edb2
Chris Correll <crcorrell@gmail.com>
2020-12-13 15:26:49 -0600

    search index

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -13,7 +13,7 @@ var relativeSizeColumn = "Relative Size";
 // Database reference
 var databaseCollectionName = "features";
 // JSON search
-var searchIndex = "https://find-my-landlord.nyc3.cdn.digitaloceanspaces.com/search-index-11-30-2020-06-30-PM.json";
+var searchIndex = "https://github.com/funkonaut/find-my-landlord/search_index_10_1.json?raw=true";
 // Map tiles
 //var tiles = "https://find-my-landlord.nyc3.cdn.digitaloceanspaces.com/features-08-09-2020-05-48-PM/{z}/{x}/{y}.pbf";
 
